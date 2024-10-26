665b419ce00ef7ee8fcc32aa1ac39ab1f93aeb3a
DavidCepeda13 <j.cepedab@uniandes.edu.co>
2024-10-26 14:14:53 -0500

    Taller

---
+++ src/app/serie/series-list/series-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Serie } from '../serie';
+import { SerieService } from '../serie.service';
+
+@Component({
+  selector: 'app-series-list',
+  templateUrl: './series-list.component.html',
+  styleUrls: ['./series-list.component.css']
+})
+export class SeriesListComponent implements OnInit {
+  /* <Atributos> */
+  series: Array<Serie> = [];
+  seasonsAverage: number = 0;
+
+  constructor(private serieService: SerieService) { }
+
+  /* <Métodos> */
+  getSeries(): void{
+    this.serieService.getSeries().subscribe(
+      (series) => {
+        this.series = series;
+
+        let count: number = 0;
+        console.log(`Pasa aquí`);
+        series.forEach(serie => {
+          count++;
+          this.seasonsAverage += serie.seasons;
+        });
+        this.seasonsAverage /= count;
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.getSeries();
+ 
+  }
+
+}
