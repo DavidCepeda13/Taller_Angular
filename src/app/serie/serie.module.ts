82cb3e2dabbd25ad1040e87c904c84a4df7d033b
DavidCepeda13 <j.cepedab@uniandes.edu.co>
2024-11-17 21:06:44 -0500

    Init

--- src/app/serie/serie.module.ts
+++ src/app/serie/serie.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SeriesListComponent } from './series-list/series-list.component';
+import { SeriesDetailComponent } from './series-detail/series-detail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [SeriesListComponent],
-  declarations: [SeriesListComponent]
+  declarations: [SeriesListComponent, SeriesDetailComponent]
 })
 export class SerieModule { }
